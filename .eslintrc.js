80ff32ade5f339e2c9fadae0bb56a331ccd8bed0
Rustam Laykulov <roos1788@gmail.com>
2020-09-12 03:21:45 +0500

    feat: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"commonjs": true,
+		"es2021": true
+	},
+	"extends": "eslint:recommended",
+	"parserOptions": {
+		"ecmaVersion": 12
+	},
+	"rules": {
+		'no-console': 'error',
+		'space-before-function-paren': ['error', 'always'],
+		'no-use-before-define': ['error', { functions: false }],
+		'arrow-parens': ['error', 'always'],
+		'brace-style': ["error", "1tbs"],
+		'no-underscore-dangle': 'off',
+		'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'never',
+    }],
+	}
+};
---
+++ config/config.default.js
@@ -0,0 +1,11 @@
+module.exports = {
+  services: {
+    dbmodels: {
+      db: 'db',
+      dialect: 'type',
+      user: 'user',
+      password: 'password',
+      host: 'host',
+    },
+  },
+};
