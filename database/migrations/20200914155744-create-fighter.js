2a453070f8143a1246aff8b71eda6681c45ff7b4
Rustam Laykulov <roos1788@gmail.com>
2020-09-14 21:53:36 +0500

    feat: fighter db model added

---
+++ database/migrations/20200914155744-create-fighter.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Fighters', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      nick_name: {
+        type: Sequelize.STRING,
+      },
+      age: {
+        type: Sequelize.INTEGER,
+      },
+      height: {
+        type: Sequelize.STRING,
+      },
+      weight: {
+        type: Sequelize.INTEGER,
+      },
+      wins: {
+        type: Sequelize.INTEGER,
+      },
+      losses: {
+        type: Sequelize.INTEGER,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Fighters');
+  },
+};
