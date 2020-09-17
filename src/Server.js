282782384322e360a9c75d21709c120bfff50f56
Rustam Laykulov <roos1788@gmail.com>
2020-09-17 23:10:01 +0500

    feat: server init

---
+++ src/Server.js
@@ -0,0 +1,23 @@
+let instanceCount = 0;
+
+class Server {
+  constructor(express) {
+    if(instanceCount) {
+      throw new Error('Server class is a singleton');
+    }
+    instanceCount++;
+
+    this.app = express();
+    this.PORT = process.env.PORT || 5000;
+  }
+
+  run() {
+    this.app.get('/', (req, res) => {
+      res.send('Testing server');
+    });
+
+    this.app.listen(this.PORT, () => console.log(`App is running on ${this.PORT}`));
+  }
+}
+
+module.exports = Server;
